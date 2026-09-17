cea2a187f9b0a3573904d0ad62cda9184cb77df3
Fardin-Faruq <fardinfaruq05@gmail.com>
2026-09-17 21:35:10 +0530

    Modifying app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 with open("report.txt", "w") as f:
     f.write("Student Management Report\n")
-    f.write("Total Students: 120\n")
+    f.write("Total Students: 200\n")
     f.write("Active Students: 45\n")
 
 print("Report generated.")
